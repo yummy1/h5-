7d670e2888163b8fcb60021e61eac1a3b9c603a9
maomao <1431608414@qq.com>
2016-11-03 16:44:39 +0800

    购物车加入、js还没运用

---
+++ cart.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ruituo on 16/11/3.
+ */
+window.onload = function () {
+    var win = document.getElementById("jd_win");
+    win.style.display = "none";
+}
+
+function show() {
+    var win = document.getElementById("jd_win");
+    win.style.display = "block";
+}
+function hidden() {
+    var win = document.getElementById("jd_win");
+    win.style.display = "none";
+}
